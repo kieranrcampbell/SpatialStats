5e4146baaff116d95a2c6346923c93310fd11e78
Kieran Campbell <kieranrcampbell@gmail.com>
2014-06-04 15:44:29 +0100

    Changed SpatialPRo -> SPData

---
+++ R/SPData.R
@@ -0,0 +1,152 @@
+## spatialpro R class
+## kieranrcampbell@gmail.com
+
+SPData <- setClass("SPData",
+                       slots = c(n.cells = "integer",
+                           n.proteins = "integer",
+                           protein.names = "character",
+                           Y = "matrix",
+                           X = "list"))
+
+#' Extracts the cell proteomics data
+#' @export
+setMethod("cells", "SPData", function(object) object@Y )
+
+#' Returns the number of cells in the sample
+#' @export
+setMethod("nCells", "SPData", function(object) dim(object@Y)[1] )
+
+#' Returns the number of proteins measured (number of channels)
+#' @export
+setMethod("nProt", "SPData", function(object) dim(object@Y)[2] )
+
+#' Returns the names of the proteins measured
+#' @export
+setMethod("pNames", "SPData", function(object) object@protein.names )
+
+#' Returns a list of nearest neighbour readouts
+#'
+#' The ith entry is an n by m matrix, for cell i having n neighbours
+#' each of which have m channels
+#' @export
+setMethod("NN", "SPData", function(object) object@X )
+
+#' @export
+setMethod("show", "SPData", function(object) {
+    cat("An object of class ", class(object), "\n",sep="")
+    cat(" ", nCells(object), " cells with ", nProt(object), " protein(s)\n", sep="")
+    invisible(NULL)
+
+})
+
+
+setValidity("SPData", function(object) {
+    msg <- NULL
+    valid <- TRUE
+    if(nCells(object) != length(NN(object))) {
+        print(nCells(object))
+        valid <- FALSE
+        msg <- c(msg, "Nearest neighbour data not available for all cells")
+    }
+
+    if(nCells(object) != dim(cells(object))[1]) {
+        valid <- FALSE
+        msg <- c(msg, "Number of cells must be equal to number of rows in cell by protein matrix")
+    }
+
+    if(nProt(object) != dim(cells(object))[2]) {
+        valid <- FALSE
+        msg <- c(msg, "Number of proteins must be equal to number of columns in cell by protein matrix")
+    }
+
+    if(valid) TRUE else msg
+
+})
+
+#' @export
+setMethod("[", "SPData", function(x,i,drop="missing") {
+    .n.proteins <- length(i)
+    .protein.names <- pNames(x)[i]
+    .Y <- NULL
+
+    if(length(i) > 1) .Y <- cells(x)[,i] else .Y <- as.matrix(cells(x)[,i])
+
+    .X <- lapply(NN(x), function(nn.cells) {
+        if(is.matrix(nn.cells)) nn.cells[,i] else as.matrix(nn.cells[i])
+    })
+
+    SPData(protein.names = .protein.names,
+               Y = .Y,
+               X = .X)
+})
+
+
+
+#' Loads an Xell matlab file into the SPData format
+#'
+#' This function parses the matlab files, pulling out relevant proteins
+#' in the 'D' channel and validates that the correct proteins are present.
+#'
+#' @param filename The matlab file
+#' @param control.isotopes The isotopes used for control to exclude from analysis
+#' @param log.data Boolean of whether to log the data
+#' @export
+loadCells <- function(filename, control.isotopes = c("Xe131","Cs133","Ir193"),
+                        log.data=TRUE) {
+    library(R.matlab)
+
+
+    ## loads relevant data from matlab and parses into list
+
+    m <- readMat(filename)
+
+    n.cells <- dim(m$Xell)[1]
+    n.proteins <- -1
+
+    ## xcolheads shows the data has the structure of a time cell, then three cells
+    ## for each protein (here we use the D channel). The first two and last measurement
+    ## are control isotopes, which we disregard.
+    xcolheads <- as.character(m$Xell.nearest.col)
+
+    xp.id <- grep(")D",xcolheads) ## x protein ids
+
+    ## remove control isotopes
+    xp.id <- setdiff(xp.id, grep(paste(control.isotopes, collapse="|"),xcolheads))
+    xprotein.names <- xcolheads[xp.id]
+    n.proteins <- length(xprotein.names)
+
+    ## now get cell-by-cell protein data
+
+    ycolheads <- as.character(m$Xell.list.col)
+    yp.id <- grep(")D", ycolheads)
+    yp.id <- setdiff(yp.id, grep(paste(control.isotopes, collapse="|"),ycolheads))
+    yprotein.names <- ycolheads[yp.id]
+
+    if(!all.equal(yprotein.names,xprotein.names)) {
+        stop("Mismatch in X and Y protein names")
+    }
+    protein.names <- xprotein.names
+
+    Y <- m$Xell.list
+    Y <- Y[,yp.id]
+
+    if(log.data) Y <- log(Y)
+
+    get.nn.count <- function(x,p.id) {
+        ## returns the nearest neighbour count for proteins
+        if(dim(x)[1] == 1) return(x[p.id])
+        x <- x[,p.id]
+        x
+    }
+
+    ## get nearest count data for ind
+    nn.count <- m$Xell.nearest
+
+    X <- lapply(nn.count, get.nn.count, xp.id)
+
+    if(log.data) X <- lapply(X, log)
+
+    sp <- SPData(protein.names=protein.names,
+                     Y=Y, X=X)
+    return( sp )
+}
