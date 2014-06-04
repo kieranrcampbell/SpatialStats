2638017d30246d6b13ae5d700cbe506a7f50c6f7
Kieran Campbell <kieranrcampbell@gmail.com>
2014-06-04 14:49:31 +0100

    First working version

---
+++ SpatialPRo/R/AllGenerics.R
@@ -0,0 +1,11 @@
+
+## extracts the list of cell readouts for a SpatialPRo object
+setGeneric("cells", function(object) standardGeneric("cells"))
+
+setGeneric("nCells", function(object) standardGeneric("nCells"))
+
+setGeneric("nProt", function(object) standardGeneric("nProt"))
+
+setGeneric("pNames", function(object) standardGeneric("pNames"))
+
+setGeneric("NN", function(object) standardGeneric("NN"))
